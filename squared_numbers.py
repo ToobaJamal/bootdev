7ae9cff9584f68be6b8c2607c1fb98ae8269936c
Tooba Jamal <52610124+ToobaJamal@users.noreply.github.com>
2023-05-04 19:55:52 +0500

    Create squared_numbers.py

---
+++ squared_numbers.py
@@ -0,0 +1,11 @@
+'''
+Write a loop that calculates and prints the squares of the numbers 100 - 109.
+
+Use the following format to print each line:
+
+NUM squared = SQUARE
+Where NUM is the number, and SQUARE is its square.
+'''
+
+for i in range(100, 110):
+    print(f"{i} squared = {i ** 2}")
