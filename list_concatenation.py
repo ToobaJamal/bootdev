beae8ef888ccb32e3c3248c580b3a1b64149877c
Tooba Jamal <52610124+ToobaJamal@users.noreply.github.com>
2023-05-06 20:04:06 +0500

    Create list_concatenation.py

---
+++ list_concatenation.py
@@ -0,0 +1,15 @@
+'''
+Create a new list that has John's favorites, followed by Jack's, followed by Breanna's.
+Print the total number of items using the len function
+Print the actual list containing all the items
+'''
+
+johns_favorites = ["sword", "shield"]
+jacks_favorites = ["potion", "hat"]
+breannas_favorites = ["feather", "lance"]
+
+# Don't touch above this line
+
+favorites = johns_favorites + jacks_favorites + breannas_favorites
+print(len(favorites))
+print(favorites)
