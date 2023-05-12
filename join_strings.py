3060e4d80b5bb10aef69e1948c07bdf560af7733
Tooba Jamal <52610124+ToobaJamal@users.noreply.github.com>
2023-05-12 21:16:37 +0500

    Create join_strings.py

---
+++ join_strings.py
@@ -0,0 +1,6 @@
+'''
+Write a function called join_strings() that concatenates all of its string inputs end-to-end, in order, and adds a comma between them.
+'''
+
+def join_strings(strings):
+    return ','.join(strings)
