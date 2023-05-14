816f815087d91d7683a35e1c4d294b47ff7e86de
Tooba Jamal <52610124+ToobaJamal@users.noreply.github.com>
2023-05-14 14:13:23 +0500

    Delete Python/intro directory

--- Python/intro/sum_of_n.py
+++
@@ -1,9 +0,0 @@
-'''
-Write a function called number_sum(n) that adds up all the numbers from 1 to n.
-'''
-
-def number_sum(n):
-    sum = 0
-    for i in range(n + 1):
-        sum += i
-    return sum
